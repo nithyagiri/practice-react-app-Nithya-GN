b988bca5ea0d50c7e9565e4a83d9ff2266da5bfe
nithya <nithi.coolz@gmail.com>
2025-10-26 19:21:36 -0500

    ProfileCreated

---
+++ profile-react-app/src/components/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProfileCard({name, age, bio}){
+    return 
+     <div>
+        <h3>name: {name}</h3>;
+        <p>age: {age}</p>;
+        <p>bio: {bio}</p>;
+     </div>
+}
+
+export default ProfileCard;
